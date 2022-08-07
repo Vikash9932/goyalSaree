97b4c9cd2378fed2de6c6f349083c33e95531695
Vikash Kumar <vikash9932@gmail.com>
2022-08-07 14:22:30 +0530

    Bug: adding master data

--- App.js
+++ App.js
@@ -1,46 +1,6 @@
-import * as React from "react";
-import { NavigationContainer } from "@react-navigation/native";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
-// import { LogBox } from "react-native";
+import * as React from 'react';
 
-//import different Screens
-import AddAdhat from "./screens/AddAdhat";
-import AddMasterData from "./screens/AddMasterData";
-import Homepage from "./screens/Homepage";
-import AddCompany from "./screens/AddCompany";
-import AddFirm from "./screens/AddFirm";
-import AddCategory from "./screens/AddCategory";
-import AddSubCategory from "./screens/AddSubCategory";
-
-const Stack = createNativeStackNavigator();
-
-const App1 = () => {
-  // React.useEffect(() => {
-  //   LogBox.ignoreLogs(true);
-  // }, []);
-
-  return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="Dashboard">
-        <Stack.Screen
-          name="Dashboard"
-          component={Homepage}
-          options={{ title: "Goyal Saree" }}
-        />
-        <Stack.Screen name="MasterData" component={AddMasterData} />
-        <Stack.Screen name="Adhat" component={AddAdhat} />
-        <Stack.Screen name="Category" component={AddCategory} />
-        <Stack.Screen name="Company" component={AddCompany} />
-        <Stack.Screen name="Firm" component={AddFirm} />
-        <Stack.Screen name="SubCategory" component={AddSubCategory} />
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
-};
-
-// export default App;
-
-import Routes from "./navigation/index";
+import Routes from './navigation/index';
 
 export default function App() {
   return <Routes />;
--- components/ViewData.js
+++ components/ViewData.js
@@ -1,11 +1,11 @@
-import React, { useState, useEffect } from "react";
-import { StyleSheet, Text, View, Keyboard } from "react-native";
-import { Picker } from "@react-native-picker/picker";
-import { db } from "../firebase.config";
-import MyButton from "./MyButton";
-import _ from "lodash";
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, Text, View, Keyboard } from 'react-native';
+import { Picker } from '@react-native-picker/picker';
+import { db } from '../firebase.config';
+import MyButton from './MyButton';
+import _ from 'lodash';
 import { AntDesign } from '@expo/vector-icons';
-import SelectDropdown from 'react-native-select-dropdown'
+import SelectDropdown from 'react-native-select-dropdown';
 
 const ViewData = ({ type, value, setValue, navigation }) => {
   const [data, setData] = useState([]);
@@ -29,7 +29,7 @@ const ViewData = ({ type, value, setValue, navigation }) => {
     if (item.Name === value) {
       defValueIndex = index;
     }
-  })
+  });
   // console.log("view data", data, value, defValueIndex)
 
   return (
@@ -39,24 +39,25 @@ const ViewData = ({ type, value, setValue, navigation }) => {
       </View>
       <View style={styles.viewStyle2}>
         <SelectDropdown
-          defaultButtonText="Select"
-          renderDropdownIcon={() => <AntDesign name="down" size={20} color="black" />}
+          defaultButtonText='Select'
+          renderDropdownIcon={() => (
+            <AntDesign name='down' size={20} color='black' />
+          )}
           defaultValueByIndex={defValueIndex}
-
           buttonStyle={styles.pickerStyle}
           data={data}
           onSelect={(selectedItem, index) => {
-            setValue(selectedItem);
+            setValue(selectedItem.Name);
           }}
           buttonTextAfterSelection={(selectedItem, index) => {
             // text represented after item is selected
             // if data array is an array of objects then return selectedItem.property to render after item is selected
-            return selectedItem.Name
+            return selectedItem.Name;
           }}
           rowTextForSelection={(item, index) => {
             // text represented for each item in dropdown
             // if data array is an array of objects then return item.property to represent item in dropdown
-            return item.Name
+            return item.Name;
           }}
         />
         {/* <Picker
@@ -86,7 +87,7 @@ const ViewData = ({ type, value, setValue, navigation }) => {
         onPress={() => {
           navigation.navigate(type);
         }}
-        title="New"
+        title='New'
       />
     </View>
   );
@@ -94,52 +95,52 @@ const ViewData = ({ type, value, setValue, navigation }) => {
 
 const styles = StyleSheet.create({
   viewParentStyle: {
-    flexDirection: "row",
-    justifyContent: "space-between",
+    flexDirection: 'row',
+    justifyContent: 'space-between',
     margin: 10,
     height: 45,
     borderRadius: 5,
     marginBottom: 2,
-    backgroundColor: "#666",
+    backgroundColor: '#666',
   },
   viewStyle1: {
     flex: 3,
-    backgroundColor: "#666",
+    backgroundColor: '#666',
     borderRadius: 5,
   },
   viewStyle2: {
-    backgroundColor: "#AAA",
+    backgroundColor: '#AAA',
     flex: 5,
     borderRadius: 5,
   },
   buttonStyle1: {
     borderRadius: 5,
     flex: 2,
-    backgroundColor: "#0000ff",
+    backgroundColor: '#0000ff',
     marginLeft: 10,
   },
   textStyle: {
-    color: "white",
+    color: 'white',
     fontSize: 16,
-    fontWeight: "900",
-    textAlign: "center",
-    height: "100%",
-    textAlignVertical: "center",
+    fontWeight: '900',
+    textAlign: 'center',
+    height: '100%',
+    textAlignVertical: 'center',
   },
   pickerStyle: {
     fontSize: 16,
-    fontWeight: "900",
-    height: "100%",
-    backgroundColor: "#AAA",
+    fontWeight: '900',
+    height: '100%',
+    backgroundColor: '#AAA',
     borderRadius: 5,
   },
   buttonStyle2: {
-    color: "#ffffff",
+    color: '#ffffff',
     fontSize: 16,
-    fontWeight: "900",
-    textAlign: "center",
-    height: "100%",
-    textAlignVertical: "center",
+    fontWeight: '900',
+    textAlign: 'center',
+    height: '100%',
+    textAlignVertical: 'center',
   },
 });
 
